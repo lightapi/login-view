a3dfe01486a1623d751f68bd6f143e3ca38912a6
Steve Hu <stevehu@gmail.com>
2024-11-12 21:03:05 -0500

    fixes #135 add local build in package.json and deploy-portal-loc.sh

--- deploy-portal-loc.sh
+++ deploy-portal-loc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Build the view in in test mode"
-yarn build
+yarn build-tst
 echo "Build completed in build folder, start copying to local folder"
-rm -rf /home/steve/lightapi/portal-config-loc/light-gateway/signin/build
-cp -r ./build /home/steve/lightapi/portal-config-loc/light-gateway/signin
+rm -rf /home/steve/lightapi/portal-config-loc/light-gateway/signin/dist
+cp -r ./dist /home/steve/lightapi/portal-config-loc/light-gateway/signin
 echo "Copied!"
