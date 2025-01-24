ecc662c61116b92faa668f0ba16a588b6e5dc4af
Steve Hu <stevehu@gmail.com>
2025-01-24 16:44:30 -0500

    fixes #6 update the deploy-portal-loc.sh and upgrade vite

--- deploy-portal-loc.sh
+++ deploy-portal-loc.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo "Build the view in in test mode"
-yarn build-tst
+echo "Build the view"
+yarn build
 echo "Build completed in build folder, start copying to local folder"
-rm -rf /home/steve/lightapi/portal-config-loc/light-gateway/signin/dist
-cp -r ./dist /home/steve/lightapi/portal-config-loc/light-gateway/signin
+rm -rf /home/steve/lightapi/portal-config-loc/all-in-one/light-gateway/signin/dist
+cp -r ./dist /home/steve/lightapi/portal-config-loc/all-in-one/light-gateway/signin
+rm -rf /home/steve/lightapi/portal-config-loc/all-in-one-light/light-gateway/signin/dist
+cp -r ./dist /home/steve/lightapi/portal-config-loc/all-in-one-light/light-gateway/signin
 echo "Copied!"
